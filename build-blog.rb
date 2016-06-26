7ad435404dc441104f05e99b58b741a3fb5e55e4
lzhrsmile <lzhrsmile@gmail.com>
2016-06-26 12:26:38 +0800

    token

--- build-blog.rb
+++
@@ -1,9 +0,0 @@
-require 'travis'
-
-Travis.access_token = "3oBnwVWRP0feUjtUQFGNVQ"
-puts "Hello #{Travis::User.current.name}!"
-
-rails = Travis::Repository.find('lzhr/Blog-Hexo-Travis-CI')
-puts "oh no" unless rails.green?
-
-rails.last_build.restart
